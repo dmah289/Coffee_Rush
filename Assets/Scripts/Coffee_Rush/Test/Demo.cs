b5021005e83c2851f51add8e6be7e7a94afbe91a
manhnd <manhnd.ds.gd@gmail.com>
2025-07-08 23:51:54 +0700

    sync

---
+++ Assets/Scripts/Coffee_Rush/Test/Demo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Coffee_Rush.Test
+{
+    public class Demo : MonoBehaviour
+    {
+        [SerializeField] private AssetReferenceGameObject go;
+        GameObject instantiatedObject;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+                go.InstantiateAsync().Completed += OnAssetInstantiated;
+            if(Input.GetKeyDown(KeyCode.A))
+                go.LoadAssetAsync().Completed += OnAssetLoaded;
+            if(Input.GetKeyDown(KeyCode.R))
+                // Just applied to the instantiated object by InstantiateAsync() method
+                go.ReleaseInstance(instantiatedObject);
+        }
+        
+        private void OnAssetLoaded(AsyncOperationHandle<GameObject> handle)
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+                instantiatedObject = Instantiate(handle.Result);
+            else Debug.LogError(handle.Status.ToString());
+        }
+
+        private void OnAssetInstantiated(AsyncOperationHandle<GameObject> handle)
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+                instantiatedObject = handle.Result;
+            else Debug.LogError(handle.Status.ToString());
+        }
+    }
+}
